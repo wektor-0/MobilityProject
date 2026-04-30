0f7411147333bb55604ebcfe0226dd2b9eabad7d
Jan-ops-ops <huppertz.jn@gmail.com>
2026-04-30 08:32:52 +0200

    Rausnehmen von Passwort aufgrund von Zeit

--- program/EFahrzeug.cs
+++ program/EFahrzeug.cs
@@ -72,7 +72,8 @@ namespace program
         }
 
         public int GetAkkustand()
-            { return _akkustand; }
+            { return _akkustand; 
+        }
 
         public bool IstVerfuegbar()
             { return this.Status.ToLower() == "bereit" && this.Akkustand > 10; 
